804565c4ee0688d20b3058e77f49407435398b49
watabean <watabean@gmail.com>
2024-12-09 22:11:23 +0900

    feat: generate sitemap using vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [react()],
+  base: "/bgg-explorer/",
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: "https://watabean.github.io",
+      basePath: "/bgg-explorer",
+    }),
+  ],
 });
